827ad8d56131b385ed2597eb2e65aef0f0eeeb78
Vignesh <vignesh.uma@gmail.com>
2017-02-25 02:44:34 +0000

    updates to the webcam panel

--- SotonHack17/src/TakePicture.java
+++ SotonHack17/src/TakePicture.java
@@ -6,18 +6,23 @@ import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
 
-public class TakePicture extends JPanel{
+public class TakePicture extends WebcamPanel{
+
+	public TakePicture(Webcam webcam) {
+		super(webcam);
+	}
 
 	public void startCamera() throws InterruptedException, IOException {
 		
-		Webcam webcam = Webcam.getDefault();
-		webcam.setViewSize(WebcamResolution.VGA.getSize());
-
-		WebcamPanel panel = new WebcamPanel(webcam);
-		panel.setFPSDisplayed(true);
-		panel.setDisplayDebugInfo(true);
-		panel.setImageSizeDisplayed(true);
-		panel.setMirrored(true);
+		//Add the three lines below and pass in 'webcam' as constructor when creating an object of this class
+		//Webcam webcam = Webcam.getDefault();
+		//webcam.setViewSize(WebcamResolution.VGA.getSize());
+		//WebcamPanel panel = new TakePicture(webcam);
+		
+		this.setFPSDisplayed(true);
+		this.setDisplayDebugInfo(true);
+		this.setImageSizeDisplayed(true);
+		this.setMirrored(true);
 		
 	}
 }
